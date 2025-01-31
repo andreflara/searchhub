58fa9c4dfff7b1abd2bb7916ae7e0c848686d43c
Andre Fernando Lara <andrefernandolara@hotmail.com>
2025-01-30 22:26:22 -0300

    corrigindo codigo

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { ChevronRight, ExternalLink, Plus, X } from "lucide-react";
+import { ExternalLink, Plus, X } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 
