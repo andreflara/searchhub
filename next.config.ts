558f4a297bd332b4d3b6692bb335afa881d19f7a
Andre Fernando Lara <andrefernandolara@hotmail.com>
2025-01-31 13:51:00 -0300

    teste 6

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -12,6 +13,17 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false
+      };
+    }
+    return config;
+  }
 }
 
-module.exports = nextConfig
+export default nextConfig;
